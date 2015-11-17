50fdb3447a6ee4919b6f146f2078137c6edbc33c
Anton Skorokhod <anton@nsl.cz>
2015-11-17 23:24:06 +0100

    2 simple example/tests

---
+++ prefixsearch_test.go
@@ -0,0 +1,48 @@
+package prefixsearch_test
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/kanocz/go-prefixsearch"
+)
+
+func ExampleAutoComplete() {
+	st := prefixsearch.New()
+	st.Add("Hello world!", 1)
+	st.Add("New impressions", 2)
+	st.Add("Hello golang!", 3)
+	st.Add("Привет, мир!", 4)
+	st.Add("Надо же :)", 5)
+
+	// converting []interface{} to []int to use sort.Ints function :)
+	intResult := []int{}
+	for _, x := range st.AutoComplete("HE") {
+		intResult = append(intResult, x.(int))
+	}
+
+	sort.Ints(intResult)
+	fmt.Println(intResult)
+	// Output: [1 3]
+}
+
+func ExampleSearch() {
+	st := prefixsearch.New()
+	st.Add("Hello world!", 1)
+	st.Add("New impressions", 2)
+	st.Add("Hello golang!", 3)
+	st.Add("Привет, мир!", 4)
+	st.Add("Надо же :)", 5)
+
+	fmt.Println(st.Search("HE"))
+	fmt.Println(st.Search("HELLO WORLD"))
+	fmt.Println(st.Search("HELLO WORLD!"))
+	fmt.Println(st.Search("HELLO WORLD!!"))
+
+	// Output:
+	// <nil>
+	// <nil>
+	// 1
+	// <nil>
+
+}
